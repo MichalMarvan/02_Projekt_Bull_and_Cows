191970883e4e5d9e362b92f3e143d40d8db5f8a1
MichalMarvan <marvan.michal@gmail.com>
2024-07-02 22:57:09 +0200

    Edit

---
+++ README.md
@@ -0,0 +1,30 @@
+Bulls and Cows (Hádání čísel)
+Cíl hry:
+Cílem hry je uhodnout tajné číslo, které si zvolil protihráč, pomocí postupného hádání a analýzy odpovědí ve formě "Bulls" a "Cows".
+
+Pravidla hry:
+
+Volba tajného čísla: Jeden hráč (Hráč 1) si zvolí tajné číslo. Toto číslo má pevně danou délku (například 4 číslice) a každá číslice musí být unikátní.
+
+Hádání: Druhý hráč (Hráč 2) se snaží toto číslo uhodnout tím, že postupně navrhuje různé kombinace čísel.
+
+Hodnocení pokusů:
+
+Bull (býk): Pokud je hádaná číslice správná a na správné pozici, je to označeno jako "Bull".
+Cow (kráva): Pokud je hádaná číslice správná, ale na nesprávné pozici, je to označeno jako "Cow".
+Odpovědi: Po každém pokusu Hráče 2 Hráč 1 poskytne zpětnou vazbu v podobě počtu Bulls a Cows. Například:
+
+Tajné číslo: 4271
+Hádání: 1234
+Odpověď: 1 Bull, 2 Cows (1 Bull za správně umístěnou číslici '2' a 2 Cows za číslice '1' a '4' na nesprávných pozicích)
+Vítězství: Hra pokračuje, dokud Hráč 2 neuhodne celé tajné číslo (tj. nezíská odpověď se 4 Bulls, pokud je délka čísla 4).
+
+Příklad hry:
+
+Tajné číslo: 5678
+Hádání 1: 1234
+Odpověď: 0 Bulls, 0 Cows
+Hádání 2: 5671
+Odpověď: 3 Bulls, 0 Cows
+Hádání 3: 5678
+Odpověď: 4 Bulls (Hráč 2 vyhrává)
--- funkce.py
+++ funkce.py
@@ -1,57 +1,88 @@
 import random
-def random_cislo (cislo:int):
 
-  cisla = []
-  for ciselny_list in range(0, 10):
-    cisla.append(str(ciselny_list))
-  
-  n = 0
-  vysledek = []
-  while n < cislo:
-    nahodne_cislo = random.choice(cisla)
-    if n == 0 and nahodne_cislo == "0":
-      n = 0
-      continue
-    elif nahodne_cislo in cisla:
-      vysledek.append(nahodne_cislo)
-      cisla.remove(nahodne_cislo)   
-      n += 1
-  
-  celek = "".join(vysledek)
-  return(celek)
+def random_cislo (delka_random_cisla:int): # -> str
+    """
+    Tato funkce vytvoří náhodné unikátní číslo, které nesmí začínat nulou
+    a každé číslo je obsaženo pouze jednou.
+    """
+    # vytvoří list čísel od 0 do 9, šlo by zadat i samostatnym listem
+    cisla = []
+    for ciselny_list in range(0, 10):
+        cisla.append(str(ciselny_list))
+    # vytvoření listu náhodných čísel, počet je dán zadanou délkou čísla
+    n = 0
+    vysledek = []
 
-def trefa (uzivatelske_cislo:str, nahoda:str): # -> str
-  i = 0
-  pocet_bull = 0
-  pocet_cow = 0
-  for cisla in uzivatelske_cislo:
-    if cisla == nahoda[i]:
-      pocet_bull +=1
-    elif cisla in nahoda:
-      pocet_cow +=1
-    i += 1
+    while n < delka_random_cisla:
+        nahodne_cislo = random.choice(cisla)
+        # podmínka k odstranění 0 z prvního místa
+        if n == 0 and nahodne_cislo == "0":
+            n = 0
+            continue
+        # vytvoření listu s náíhodnými čísly
+        elif nahodne_cislo in cisla:
+            vysledek.append(nahodne_cislo)
+            cisla.remove(nahodne_cislo)   
+            n += 1
+    # předělání listu s jednotlivými čísly na jedno číslo
+    celek = "".join(vysledek)
+    return(celek)
 
-  if pocet_bull in (0, 1):
-    a = "bull"
-  else:
-    a = "bulls"
+def trefa (uzivatelske_cislo:str, nahodne_cislo:str): # -> list
+    """
+    Funkce pro kontrolu zda zadané číslo je stejné jako náhodné číslo. 
+    Vypsání bull a cow pro jednotlivé typy.
+    """
+    print(uzivatelske_cislo)
+    print(nahodne_cislo)
+    # potřebné zadání pro for cyklus   
+    i = 0
+    pocet_bull = 0
+    pocet_cow = 0
+    # počty bull/cow
+    for cisla in uzivatelske_cislo:
+        if cisla == nahodne_cislo[i]:
+            pocet_bull +=1
+        elif cisla in nahodne_cislo:
+            pocet_cow +=1
+        i += 1
+    # rozdělení jednotného a množného čísla pro bull
+    if pocet_bull in (0, 1):
+        bull = "bull"
+    else:
+        bull = "bulls"
+    # rozdělení jednotného a množného čísla pro cow    
+    if pocet_cow in (0, 1):
+        cow = "cow"
+    else:
+        cow = "cows"
+    
+    return([str(pocet_bull), str(pocet_cow), bull, cow])
 
-  if pocet_cow in (0, 1):
-    b = "cow"
-  else:
-    b = "cows"
-  
-  return([str(pocet_bull), str(pocet_cow), a, b])
+def vyhodnoceni (pocet_pokusu:int): # -> str
+    """
+    Funkce pro vypsání vyhodnocení výsledků podle počtu pokusů.
+    """
+    # vyhodnocení dle počtu pokusů a vypsání stringu jako hodnocení pokusu
+    if pocet_pokusu == 1:
+        vyhodnoceni_vysledku = "Neuvěřitelné, máš velké štěsté!"
+    elif pocet_pokusu in (2, 3):
+        vyhodnoceni_vysledku = "Neskutečný počin."
+    elif pocet_pokusu in (4, 5):
+        vyhodnoceni_vysledku = "Docela dobré."
+    elif pocet_pokusu in (5, 6):
+        vyhodnoceni_vysledku = "Mohlo to být lepší."
+    else:
+        vyhodnoceni_vysledku = "Slabota, máš na víc!"
 
-def vyhodnoceni (vyhodnoceni):
-  if vyhodnoceni == 1:
-    co = "neuvěřitelné, máš velkou kliku"
-  elif vyhodnoceni in (2, 3):
-    co = "neskutečný počin"
-  elif vyhodnoceni in (4, 5):
-    co = "docela dobré"
-  elif vyhodnoceni in (5, 6):
-    co = "mohlo to být lepší"
-  else:
-    co = "slabota"
-  return(co)
+    return(vyhodnoceni_vysledku)
+
+def pocet_cyklu (pocet_cyklu:int): #-> str
+    if pocet_cyklu == 1:
+        pocet = "cyklus"
+    elif pocet_cyklu in (2, 3, 4):
+        pocet = "cykly"
+    else:
+        pocet = "cyklů"
+    
+    return(pocet)
