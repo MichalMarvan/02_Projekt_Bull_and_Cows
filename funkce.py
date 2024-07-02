7d24ba1ab7119849db99813cb03658a9a08a5895
MichalMarvan <marvan.michal@gmail.com>
2024-07-02 21:27:40 +0200

    pridani funkci

--- funkce.py
+++ funkce.py
@@ -1,5 +1,5 @@
 import random
-def random_cislo (cislo):
+def random_cislo (cislo:int):
 
   cisla = []
   for ciselny_list in range(0, 10):
@@ -19,3 +19,39 @@ def random_cislo (cislo):
   
   celek = "".join(vysledek)
   return(celek)
+
+def trefa (uzivatelske_cislo:str, nahoda:str): # -> str
+  i = 0
+  pocet_bull = 0
+  pocet_cow = 0
+  for cisla in uzivatelske_cislo:
+    if cisla == nahoda[i]:
+      pocet_bull +=1
+    elif cisla in nahoda:
+      pocet_cow +=1
+    i += 1
+
+  if pocet_bull in (0, 1):
+    a = "bull"
+  else:
+    a = "bulls"
+
+  if pocet_cow in (0, 1):
+    b = "cow"
+  else:
+    b = "cows"
+  
+  return([str(pocet_bull), str(pocet_cow), a, b])
+
+def vyhodnoceni (vyhodnoceni):
+  if vyhodnoceni == 1:
+    co = "neuvěřitelné, máš velkou kliku"
+  elif vyhodnoceni in (2, 3):
+    co = "neskutečný počin"
+  elif vyhodnoceni in (4, 5):
+    co = "docela dobré"
+  elif vyhodnoceni in (5, 6):
+    co = "mohlo to být lepší"
+  else:
+    co = "slabota"
+  return(co)
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import funkce as f
 
 delka_cisla = input("Jak dlouhé čílo chcete hádat? ")
-nahoda = f.random_cislo(5)
+nahoda = f.random_cislo(int(delka_cisla))
 print(nahoda)
+
+pocet_cyklu = 0
+while True:
+  uzivatelske_cislo = input("zadejte váš tip: ")
+  vysledek = f.trefa(uzivatelske_cislo, nahoda)
+  pocet_cyklu += 1
+  print(vysledek[0], vysledek[2], "and", vysledek[1], vysledek[3])
+  if vysledek[0] == delka_cisla:
+    print("-"*30, "Gratuluji, trefil jsi číslo!", "-"*30, sep="\n")
+    break
+    #exit()
+  else:
+    print("-"*35)
+    continue
+
+print("Zvládl jsi to na", pocet_cyklu, "cykly")
+cykly = f.vyhodnoceni(pocet_cyklu)
+print("Tvé hodnocení:", cykly
+      )
