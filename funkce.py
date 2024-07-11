2787e112dd0f522060d6045650f9589bb91df58a
MichalMarvan <marvan.michal@gmail.com>
2024-07-11 22:10:25 +0200

    oprava kódu

--- funkce.py
+++ funkce.py
@@ -1,14 +1,50 @@
 import random
+from datetime import datetime
 
-def random_cislo (delka_random_cisla:int): # -> str
+def privitani ():
     """
-    Tato funkce vytvoří náhodné unikátní číslo, které nesmí začínat nulou
-    a každé číslo je obsaženo pouze jednou.
+    Přivítání uživatele a seznámení s pravidly.
+    """
+    # přivítání a popsání programu
+    return(print("-" * 70, "Vítám tě ve hře:".center(70), "-" * 70, 
+"Bulls & Cows".center(70), "-" * 70, 
+"""Hra Bulls and Cows je logická hádací hra pro dva hráče. Cílem hry 
+je uhodnout tajné číslo, které je náhodně vygenerováno programem, 
+pomocí postupného hádání a analýzy odpovědí ve formě "Bulls" a "Cows".""".center(70), "-" * 70, 
+sep="\n"))
+
+def cisla_random_cisla () -> list:
+    """
+    Vytvoření listu, který obsahuje hodnoty, ze kterých se skládá náhodné číslo.
     """
     # vytvoří list čísel od 0 do 9, šlo by zadat i samostatnym listem
     cisla = []
     for ciselny_list in range(0, 10):
         cisla.append(str(ciselny_list))
+    
+    return cisla
+
+def hodnoty_pro_zadani() -> list:
+    """
+    Vytvoří list, který obsahuje hodnoty, které je možné zadat pro hádání. 
+    """
+    # hodnotou ve funkci range mohu udával jak dlouhé může být zadávané číslo
+    hodnoty_pro_hadani = []
+
+    for ciselny_list in range(3, 10):
+        hodnoty_pro_hadani.append(str(ciselny_list))
+        
+    return (hodnoty_pro_hadani)
+
+def random_cislo (delka_random_cisla:int) -> str:
+    """
+    Tato funkce vytvoří náhodné unikátní číslo, které nesmí začínat nulou
+    a každé číslo je obsaženo pouze jednou.
+    """
+    # vytvoří list čísel od 0 do 9, šlo by zadat i samostatnym listem
+    
+    cisla = cisla_random_cisla ()
+
     # vytvoření listu náhodných čísel, počet je dán zadanou délkou čísla
     n = 0
     vysledek = []
@@ -28,13 +64,14 @@ def random_cislo (delka_random_cisla:int): # -> str
     celek = "".join(vysledek)
     return(celek)
 
-def trefa (uzivatelske_cislo:str, nahodne_cislo:str): # -> list
+def trefa (uzivatelske_cislo:str, nahodne_cislo:str) -> list:
     """
     Funkce pro kontrolu zda zadané číslo je stejné jako náhodné číslo. 
     Vypsání bull a cow pro jednotlivé typy.
     """
-    print(uzivatelske_cislo)
-    print(nahodne_cislo)
+    #print("uzivatelske cislo je:", uzivatelske_cislo)
+    #print("nahodne cislo je", nahodne_cislo) ## KONTROLA RANDOM ČÍSLA PŘI KONTROLE KÓDU, PRO RYCHLEJŠÍ UHÁDNUTÍ ##
+    
     # potřebné zadání pro for cyklus   
     i = 0
     pocet_bull = 0
@@ -59,7 +96,7 @@ def trefa (uzivatelske_cislo:str, nahodne_cislo:str): # -> list
     
     return([str(pocet_bull), str(pocet_cow), bull, cow])
 
-def vyhodnoceni (pocet_pokusu:int): # -> str
+def vyhodnoceni (pocet_pokusu:int) -> str:
     """
     Funkce pro vypsání vyhodnocení výsledků podle počtu pokusů.
     """
@@ -77,12 +114,137 @@ def vyhodnoceni (pocet_pokusu:int): # -> str
 
     return(vyhodnoceni_vysledku)
 
-def pocet_cyklu (pocet_cyklu:int): #-> str
+def cykly_celkem (pocet_cyklu:int) -> str:
+    """
+    Funkce pro sklonování slova cvyklus.
+    """
     if pocet_cyklu == 1:
         pocet = "cyklus"
     elif pocet_cyklu in (2, 3, 4):
         pocet = "cykly"
     else:
-        pocet = "cyklů"
-    
+        pocet = "cyklů"    
     return(pocet)
+
+def zadani_hodnoty() -> list:
+    """
+    Funce se zeptá na hodnotu délky čísla a následně délku číslazkontoluje, zda je správně zadaná.
+    """
+    
+    while True: 
+        # zadání délky čísla uživatelem
+        delka_cisla = input("Jak dlouhé čílo chcete hádat? (Můžeš zadat hodnoty od 3 do 10): ")
+        # list s hodnotami, které může zadaná délka obsahovat
+        kontrola = hodnoty_pro_zadani()
+        # for cyklus pro kontrolu zda je zadaná délka číslo a zároven je délka v listu délek (hodnoty pro zadání)
+        if delka_cisla in kontrola and delka_cisla.isdigit():
+            nahodne_cislo = random_cislo(int(delka_cisla))
+            print("-" * 70)
+            break
+        else:
+            print("Zalal jsi špatnou hodnotu, zkus to znovu:")
+            continue
+
+    return(delka_cisla, nahodne_cislo)
+
+def kontrola_zadani(uzivatelske_cislo:str) -> bool:
+    """
+    Funkce projde uživatelem zadané číslo a zjistí, zda se v čísle neopakují stejné hodnoty.
+    """
+    vysledek = []
+
+    for cisla in uzivatelske_cislo:
+        if uzivatelske_cislo.count(cisla) > 1:
+            vysledek.append("Ano")
+        else:
+            vysledek.append("Ne")
+
+    if "Ano" in vysledek:
+        return True
+    else:
+        return False
+    
+def hlavni_vypocet() -> list:
+    """
+    Funkce provede halvní výpočet hry Bull and Cows.
+    """
+    
+    # zadání délky čísla, které budeme hádat - funkce random_cislo
+    zadane_hodnoty = zadani_hodnoty()   
+    
+    #print(nahodne_cislo)
+    # spuštění stopek pro získání údaje jak dlouho uživatel hádal
+    spusteni_stopek = datetime.now()
+
+    # výpočet počtu pokusů
+    pocet_cyklu = 0
+    while True:
+        # zadání uživatelského čísla         
+        uzivatelske_cislo = input("zadejte váš tip: ")  
+        
+        # hlídání počtu cyků spuštění funkce while
+        pocet_cyklu += 1
+
+        # kontrola, zda se uživatel nepřepsal nebo použil dlouhé/krátké číslo, než je hádané číslo.    
+        if len(uzivatelske_cislo) != int(zadane_hodnoty[0]) or uzivatelske_cislo.isdigit() == False or kontrola_zadani(uzivatelske_cislo):
+            print("Číslo nemá správnou délku, nezadal jsi číslo nebo jsi zadal dvě stejná čísla.", "-" * 70, sep="\n")
+            continue
+
+        #kontrola čísla pomocí funkce trefa
+        vysledek = trefa(uzivatelske_cislo, zadane_hodnoty[1])
+
+        # vypsání funkce trefa
+        print(vysledek[0], vysledek[2], "and", vysledek[1], vysledek[3])
+        
+        # ukončování hry, pokud počet bull bude stejný jako délka zadaného čísla
+        if vysledek[0] == zadane_hodnoty[0]:
+            print("-"*70, "Gratuluji, trefil jsi číslo!", "-"*70, sep="\n")
+            konec_stopek = datetime.now()
+            break
+        else:
+            print("-" * 70)
+            continue
+
+    return(spusteni_stopek, konec_stopek, pocet_cyklu)
+
+def vypsani_vysledku(spusteni_stopek, konec_stopek, pocet_cyklu):
+    """
+    Funkce vypíše výsledky hry.
+    """
+    # vypsání výsledků
+    print("Zvládl jsi to na", pocet_cyklu, cykly_celkem(pocet_cyklu))
+
+    # vyhodnocení vytvořené pomocí funkce vyhodnocení
+    cykly = vyhodnoceni(pocet_cyklu)
+    print("Tvé hodnocení:", cykly)
+
+    # zapsání času, který byl potřeba na vyřešení 
+    celkovy_cas = konec_stopek - spusteni_stopek
+    print("-"*70, f"Čas spuštění programu: {celkovy_cas} sekund".center(70), "-"*70, sep="\n")
+
+def opakovani():
+    """
+    FFunkce pro zopakování hry.
+    """
+    # Zopakování programu
+    print("-" * 70,"Přejete si Hru hrát znovu?", "A >>> ANO", "Cokoli jiného >>> NE", "-" * 70, sep="\n")
+    znovu = input(">>> ")
+    print("-" * 70)
+
+    if znovu == "A":
+        hlavni_funkce()
+    else:
+        print("Ukončuji Hru", "-" * 70, sep="\n")
+        exit()
+
+def hlavni_funkce():
+    """
+    Funkce obsahuje všechny potřebné funkce ke spuštění programu.
+    """    
+    spusteni_stopek, konec_stopek, pocet_cyklu = hlavni_vypocet()
+    vypsani_vysledku(spusteni_stopek, konec_stopek, pocet_cyklu)
+    opakovani()
+
+if __name__ == "__main__":
+    privitani ()
+    hlavni_funkce()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import funkce as f
-from datetime import datetime
 
 """
 main.py: druhý projekt do Engeto Online Python Akademie
@@ -8,60 +7,6 @@ email: marvan.michal@gmail.com
 discord: Michal_M
 """
 
-# přivítání a popsání programu
-print("-" * 50, "Vítám tě ve hře:".center(50), "-" * 50, 
-"Bulls & Cows".center(50), "-" * 50, 
-"""Hra Bulls and Cows je logická hádací hra pro dva 
-hráče. Cílem hry je uhodnout tajné číslo, které je 
-náhodně vygenerováno programem, pomocí postupného 
-hádání a analýzy odpovědí ve formě "Bulls" a "Cows".""".center(50), "-" * 50, 
-sep="\n")
-
-# zadání délky čísla, které budeme hádat - funkce random_cislo
-delka_cisla = input("Jak dlouhé čílo chcete hádat? ")
-nahodne_cislo = f.random_cislo(int(delka_cisla))
-print("-" * 50)
-
-#print(nahodne_cislo)
-# spuštění stopek pro získání údaje jak dlouho uživatel hádal
-spusteni_stopek = datetime.now()
-
-# výpočet počtu pokusů
-pocet_cyklu = 0
-while True:
-    # zadání uživatelského čísla 
-    uzivatelske_cislo = input("zadejte váš tip: ")  
-        
-    # hlídání počtu cyků spuštění funkce while
-    pocet_cyklu += 1
-
-    # kontrola, zda se uživatel nepřepsal nebo použil dlouhé/krátké číslo, než je hádané číslo.    
-    if len(uzivatelske_cislo) != int(delka_cisla) or uzivatelske_cislo.isdigit() == False :
-        print("Číslo nemá správnou délku nebo jsi nezadal číslo.", "-" * 50, sep="\n")
-        continue
-
-    #kontrola čísla pomocí funkce trefa
-    vysledek = f.trefa(uzivatelske_cislo, nahodne_cislo)
-
-    # vypsání funkce trefa
-    print(vysledek[0], vysledek[2], "and", vysledek[1], vysledek[3])
-    
-    # ukončování hry, pokud počet bull bude stejný jako délka zadaného čísla
-    if vysledek[0] == delka_cisla:
-        print("-"*50, "Gratuluji, trefil jsi číslo!", "-"*50, sep="\n")
-        konec_stopek = datetime.now()
-        break
-    else:
-        print("-" * 50)
-        continue
-
-# vypsání výsledků
-print("Zvládl jsi to na", pocet_cyklu, f.pocet_cyklu(pocet_cyklu))
-
-# vyhodnocení vytvořené pomocí funkce vyhodnocení
-cykly = f.vyhodnoceni(pocet_cyklu)
-print("Tvé hodnocení:", cykly)
-
-# zapsání času, který byl potřeba na vyřešení 
-celkovy_cas = konec_stopek - spusteni_stopek
-print("-"*50, f"Čas spuštění programu: {celkovy_cas} sekund".center(50), "-"*50, sep="\n")
+if __name__ == "__main__":
+    f.privitani ()
+    f.hlavni_funkce()
